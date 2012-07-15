5c30bd4d3f306b09f588d715e2783a8b13889493
Jerry Yang <hy0kle@gmail.com>
2012-07-15 21:13:17 +0800

    Fix some code.

--- script/iterator.lua
+++ script/iterator.lua
@@ -1,15 +1,25 @@
-print_r = function(array)
+debug = {};
+debug.prefix = '';
+debug.print_r = function(array, ...)
+    local prefix = '';
+    local arg_prefix = '';
+    if arg[1] then
+        arg_prefix = arg[1];
+    end;
+    debug.prefix = debug.prefix .. arg_prefix;
+    prefix = debug.prefix;
+
     if 'table' ~= type(array) then
-        print('It is NOT table.');
+        print('It is NOT table: ' .. array);
         return 0;
     end; -- end if
 
     for key, value in pairs(array) do
         if 'table' == type(value) then
-            print(key .. ' is a table =>');
-            print_r(value);
+            print(prefix .. key .. ' is a table =>');
+            debug.print_r(value, '$$$');
         else
-            print(key .. ' = ' .. value);
+            print(prefix .. key .. ' = ' .. value);
         end;    -- end if
     end;    -- end for
 
@@ -26,6 +36,6 @@ for k, v in pairs(t_table) do
     print(k .. ' = ' .. v)
 end
 
-print_r('test function...');
-print_r(t_array);
+debug.print_r('test function...');
+debug.print_r(t_array);
 
