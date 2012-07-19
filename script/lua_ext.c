2482b7d9d8018936bc9a0462a7d74e8751756f08
Jerry Yang <hy0kle@gmail.com>
2012-07-19 16:28:34 +0800

    Add new file to ext lua.

---
+++ script/lua_ext.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#define BUFF_LEN    1024
+
+// gcc study.c -llua -lm -ldl
+
+static void print_prompt()
+{
+    printf("lua prompt>> ");
+}
+
+int main (int argc, char *argv[])
+{
+    char buff[BUFF_LEN] = {0};
+    int error;
+
+    lua_State *L = luaL_newstate(); /* opens Lua */
+    luaL_openlibs(L);
+
+    print_prompt();
+    while (fgets(buff, sizeof(buff), stdin) != NULL)
+    {
+        error = luaL_loadbuffer(L, buff, strlen(buff), "line") || lua_pcall(L, 0, 0, 0);
+
+        if (error)
+        {
+            fprintf(stderr, "%s", lua_tostring(L, -1));
+            lua_pop(L, 1);/* pop error message from the stack */
+        }
+
+        print_prompt();
+    }
+
+    lua_close(L); return 0;
+}
