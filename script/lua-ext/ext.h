a90e2c236653c4063ebae1fa5fc71a93688b42d7
Jerry Yang <hy0kle@gmail.com>
2012-07-20 10:28:46 +0800

    Add head file.

---
+++ script/lua-ext/ext.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#define BUFF_LEN    1024
+
+/* gcc lua_ext.c -llua -lm -ldl */
+/**
+ * lua: _VERSION 5.2
+ * */
+
